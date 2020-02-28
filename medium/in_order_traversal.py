ee78c74bcf893ba66d1fe8425e5cbf5656ac85e1
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-28 05:39:31 -0800

    iteratively done

--- medium/in_order_traversal.py
+++ medium/in_order_traversal.py
@@ -25,4 +25,37 @@ class TreeNode:
 class Solution:
         def inorderTraversal(self, root: TreeNode):
             
-            pass
+            output_arr = []
+            stack = []
+            # base case
+            if root == None:
+                return output_arr
+            
+            cur_node = root
+            while cur_node != None or len(stack) > 0: # goes until reaches the farthest right node
+                while cur_node != None: # goes until reaches the farthest left node
+                    stack.append(cur_node)
+                    cur_node = cur_node.left
+
+                cur_node = stack.pop()
+                output_arr.append(cur_node.val)
+                cur_node = cur_node.right
+            return output_arr
+
+            # another solution
+            # if root is None:
+            #     return []
+            # list = []
+            # stack = []
+            # ref = root
+            # while ref is not None or len(stack) > 0:
+            #     if ref is None:
+            #         ref = stack.pop()
+            #         list.append(ref.val)
+            #         ref = ref.right
+            #         continue
+            #     stack.append(ref)
+            #     ref = ref.left
+            # return list
+
+            
