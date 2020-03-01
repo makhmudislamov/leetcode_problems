9eaca3101c11e0b2011b2387d089a6b7ce074ba6
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-01 14:27:06 -0800

    added new approaches

--- easy/valid_anagram.py
+++ easy/valid_anagram.py
@@ -14,14 +14,29 @@ You may assume the string contains only lowercase alphabets.
 Follow up:
 What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
-Questions to ask. space between the letters? 
+Questions to ask >> space between the letters? is empty string valid?
 """
-
+import collections
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        for char in s:
-            if char not in s:
-                return False
+        # basic solution
+        # for char in s:
+        #     if not char.isalpha() and char not in s:
+        #         return False
+        #     return True
+
+        # better solution
+        # base cases
+        if len(s) == 0 and len(t) == 0:
             return True
+        elif len(s) != len(t):
+            return False
+
+        if sorted(s) == sorted(t):
+            return True
+
+        # dict method
+        # documentation: https://docs.python.org/3/library/collections.html
+        # return collections.Counter(s) == collections.Counter(t)
 
