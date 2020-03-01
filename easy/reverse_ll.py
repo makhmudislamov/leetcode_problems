6f6ea6b763c195b55ca201ebbb2781d36a29337e
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-29 21:08:10 -0800

    recursive pseudocode

--- easy/reverse_ll.py
+++ easy/reverse_ll.py
@@ -19,10 +19,31 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
+        # ITERATIVE
         # declare a variable to point to previous Node
         # iterate over the LL until head reaches None(tail)
             # declare a variable that points to next Node
             # point head to previous Node
             # bring previous Node pointer to head
             # move the head pointer to next Node (to move the iteration)
-        pass
+        # return previous Node
+        if not head:
+            return None 
+        previous_node = None
+        while head != None:
+            next_node = head.next
+            head.next = previous_node
+            previous_node = head
+            head = next_node
+        return previous_node
+
+        # RECURSIVE 
+        # declare a helper function that takes two ars: previous (None) and current (head) Nodes
+            # if current is None
+            # return previous Node
+            # declare a variable that points to next Node to current
+            # point next to current Node to previous Node
+            # bring previous Node to current Node
+            # bring current node pointer to next Node
+
+        # call the function recursively
