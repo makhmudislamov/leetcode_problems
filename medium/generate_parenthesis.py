051106055300b3bdcc4685f760162c0cfb779843
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-29 17:27:15 -0800

    started this exercise

--- medium/generate_parenthesis.py
+++ medium/generate_parenthesis.py
@@ -4,14 +4,17 @@ Given n pairs of parentheses, write a function to generate all combinations of w
 For example, given n = 3, a solution set is:
 
 [
-  "((()))",
-  "(()())",
-  "(())()",
-  "()(())",
-  "()()()"
+    "((()))",
+    "(()())",
+    "(())()",
+    "()(())",
+    "()()()"
 ]
 """
 
 
 def generateParenthesis(self, n: int) -> [str]:
+    # declare an empty array
+    # base cases, n = 0 and n = 1
+    # 
     pass
