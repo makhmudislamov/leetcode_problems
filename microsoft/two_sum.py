6aeddd727c1e032810d65425d0be651f70c13a50
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-11 07:23:44 -0700

    basic solution

--- microsoft/two_sum.py
+++ microsoft/two_sum.py
@@ -9,8 +9,34 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
+Questions to ask:
+1. is array sorted?
+2. can target exist in the array?
+3. only positive nums?
 """
 
 
-def twoSum(self, nums: [int], target: int) -> [int]:
-    pass
+def twoSum(nums: [int], target: int):
+
+    start = 0
+    end = len(nums) - 1
+
+    while start <= end:
+        
+        if nums[start] + nums[end] == target:
+            return [nums[start], nums[end]]
+
+        if nums[start] + nums[end] > target:
+            end -= 1
+
+        if nums[start] + nums[end] < target:
+            start += 1
+    
+
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print(twoSum(nums, target))
