a1d6eb7a97ce2cdd9542469850deb79734a881ad
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-14 17:27:03 -0700

    too much time complextity here - looping 3 times

--- microsoft/arr_and_str/set_matrix_zeroes.py
+++ microsoft/arr_and_str/set_matrix_zeroes.py
@@ -2,32 +2,31 @@
 Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
 
 Example 1:
-
 Input: 
 [
-  [1,1,1],
-  [1,0,1],
-  [1,1,1]
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]
 ]
 Output: 
 [
-  [1,0,1],
-  [0,0,0],
-  [1,0,1]
+    [1,0,1],
+    [0,0,0],
+    [1,0,1]
 ]
+
 Example 2:
-
 Input: 
 [
-  [0,1,2,0],
-  [3,4,5,2],
-  [1,3,1,5]
+    [0,1,2,0],
+    [3,4,5,2],
+    [1,3,1,5]
 ]
 Output: 
 [
-  [0,0,0,0],
-  [0,4,5,0],
-  [0,3,1,0]
+    [0,0,0,0],
+    [0,4,5,0],
+    [0,3,1,0]
 ]
 Follow up:
 
@@ -36,9 +35,46 @@ A simple improvement uses O(m + n) space, but still not the best solution.
 Could you devise a constant space solution?
 """
 
+import pprint
 def setZeroes(matrix):
     """
     :type matrix: List[List[int]]
     :rtype: None Do not return anything, modify matrix in-place instead.
     """
-    pass
+    for row_inx, row_list in enumerate(matrix):
+        if 0 in row_list:
+   
+            # all_zero_row = matrix[row_inx]
+            zero_found_row = matrix[row_inx]
+            print("this" , zero_found_row)
+            set_to_zero = row_list.index(0)
+            for row_list in matrix:
+                row_list[set_to_zero] = 0
+    
+            zero_found_row = [0] * len(zero_found_row)
+
+    # print("this again", zero_found_row)
+    
+    return matrix
+
+# def display_matrix(matrix):
+
+#     for row in matrix:
+#         for item in row:
+#             print(str(item))
+#             if item == 0:
+                
+
+
+
+
+
+
+matrix = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1]
+]
+
+# print(setZeroes(matrix))
+print(display_matrix(matrix))
