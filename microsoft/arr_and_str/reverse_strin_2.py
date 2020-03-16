2a777578cd4410da8773872b2ca944d04b823da9
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-15 18:27:24 -0700

    done

--- microsoft/arr_and_str/reverse_strin_2.py
+++ microsoft/arr_and_str/reverse_strin_2.py
@@ -1,5 +1,5 @@
 """
-Given an input string , reverse the string word by word. 
+Given an input string, reverse the string word by word. 
 
 Example:
 
@@ -13,11 +13,37 @@ The words are always separated by a single space.
 Follow up: Could you do it in-place without allocating extra space?
 """
 
+def reverse(arr, left, right):
+    # left, right = 0, len(arr)-1
+
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return arr
+
+def reverse_each_word(arr):
+    start = end = 0
+
+    # looping over the array
+    while start < len(arr):
+        # looping over the array and each word
+        while end < len(arr) and arr[end] != " ":
+            end += 1
+        reverse(arr, start, end - 1)
+        # next word
+        start = end + 1
+        end += 1
+    return arr
 
 def reverseWords(s):
     """
     :type s: List[str]
     :rtype: None Do not return anything, modify s in-place instead.
     """
-    pass
+    print(reverse(s, 0, len(s)-1))
+    print(reverse_each_word(s))
 
+
+s = ["t", "h", "e", " ", "s", "k", "y", " ", "i", "s", " ", "b", "l", "u", "e"]
+print(reverseWords(s))
