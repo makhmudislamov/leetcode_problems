08aa49a4eafa2a60bd9895f0fb82246dcdea62c0
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-16 12:06:02 -0700

    implemented the pseudocode - passes basic test

--- microsoft/arr_and_str/longest_palindromic.py
+++ microsoft/arr_and_str/longest_palindromic.py
@@ -17,13 +17,32 @@ def longestPalindrome(s):
     :type s: str
     :rtype: str
     """
-    # palindromes = {}
-
     # set two pointers
     # one stays at the start and another one starts moving toward the end
     # after each move to right check if the current string is pal
     # if so append to dictionary: pal, len
     # at the end loop over the dictionary and return the string with the longest len
     # if there is only on item in dict. return the item
+    palindromes = {}
+    end = 1
 
-   
+    while end < len(s):
+        current_string = s[:end+1]
+        # print(current_string[::-1])
+        if current_string == current_string[::-1]:
+            palindromes[current_string] = len(current_string)        
+        end += 1
+
+    max_len = max(palindromes.values())
+    for string, length in palindromes.items():
+        if length == max_len:
+            return string
+        
+
+
+
+
+
+
+s = "babad"
+print(longestPalindrome(s))
