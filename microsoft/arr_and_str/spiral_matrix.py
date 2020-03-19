532d64c1ff46222041fa3a231f5c71b4b3beacf7
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-18 18:50:01 -0700

    pseudocode

--- microsoft/arr_and_str/spiral_matrix.py
+++ microsoft/arr_and_str/spiral_matrix.py
@@ -3,7 +3,6 @@ Given a matrix of m x n elements (m rows, n columns),
 return all elements of the matrix in spiral order.
 
 Example 1:
-
 Input:
 [
     [ 1, 2, 3 ],
@@ -11,8 +10,8 @@ Input:
     [ 7, 8, 9 ]
 ]
 Output: [1,2,3,6,9,8,7,4,5]
-Example 2:
 
+Example 2:
 Input:
 [
     [1, 2, 3, 4],
@@ -20,8 +19,19 @@ Input:
     [9,10,11,12]
 ]
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+quetsions to ask:
+1) duplicates?
+2) sorted?
+
 """
 
 
 def spiralOrder(matrix: [[int]]):
+    # declare output arr
+    # keep matrix height and width as variables
+    # while iterating the matrix
+    # change direction in turning points and make sure you dont visit already visited cells
+
+    
     pass
