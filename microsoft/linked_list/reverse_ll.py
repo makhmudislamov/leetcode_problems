92147da8d8218f7149ae6ec6bf2f01ec23541b61
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-21 12:23:54 -0700

    both solutions

--- microsoft/linked_list/reverse_ll.py
+++ microsoft/linked_list/reverse_ll.py
@@ -8,6 +8,8 @@ Output: 5->4->3->2->1->NULL
 Follow up:
 
 A linked list can be reversed either iteratively or recursively. Could you implement both?
+Good explanation iterative: https://www.youtube.com/watch?v=XDO6I8jxHtA&t=502s
+
 """
 
 # Definition for singly-linked list.
@@ -19,4 +21,22 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        pass
+
+        # ITERATIVE - accepted
+        prev = None
+        while head:
+            temp = head
+            head = head.next
+            temp.next = prev
+            prev = temp
+
+        return prev
+
+        #         RECURSIVE - accepted
+        # if head is None or head.next is None:
+        #     return head
+
+        # reversed_head = self.reverseList(head.next)
+        # head.next.next = head
+        # head.next = None
+        # return reversed_head
