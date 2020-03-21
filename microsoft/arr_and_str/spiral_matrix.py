0a75c59f0b539b0d512acaad2daf253e119ca2b2
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-21 11:16:18 -0700

    started this

--- microsoft/arr_and_str/spiral_matrix.py
+++ microsoft/arr_and_str/spiral_matrix.py
@@ -34,18 +34,80 @@ def spiralOrder(matrix: [[int]]):
     # change direction in turning points and make sure you dont visit already visited cells
 
     output_arr = []
-    height = len(matrix)
-    width = len(matrix[0])
+    # height = len(matrix)
+    # width = len(matrix[0])
     
+    # for row in range(1):
+    #     for col in range(width):
+    #         cell_item = matrix[row][col]
+    #         output_arr.append(cell_item)
+    #         # if matrix[row][col] == matrix[0][len(matrix[0])-1]: # turning point
+    #         #     break
+    # last_item = len(matrix[0])-1
+    # for row in range(1, height):
+    #     output_arr.append(matrix[row][last_item])
+    #     # print("here", matrix[row][last_item])
+    
+    # for cell in matrix[height-1]:
+    #     print("rev", matrix[cell])
 
-    for row in range(height):
-        for col in range(width):
-            print(matrix[row][col])
+    # print(output_arr)
 
+    row_start = 0
+    row_end = len(matrix) - 1
+    col_start = 0
+    col_end = len(matrix[0]) - 1
+
+    while row_start <= row_end and col_start <= col_end:
+
+        for i in range(col_start, col_end + 1):
+            # print("cur item", matrix[row_start][i])
+            output_arr.append(matrix[row_start][i])
+        
+        row_start += 1
+
+        for i in range(row_start, row_end + 1):
+            # print("cur item", matrix[i][col_end])
+            output_arr.append(matrix[i][col_end])
+
+        col_end -= 1
+
+        if row_start <= row_end:
+            for i in range(col_end, col_start-1, -1):
+                # print("cur item", matrix[row_end][i])
+                output_arr.append(matrix[row_end][i])
+        
+        row_end -= 1
+
+    
+        if col_start <= col_end:
+            for i in range(row_end, row_start - 1, -1):
+                # print("cur item", matrix[i][col_start])
+                output_arr.append(matrix[i][col_start])
+        
+        col_start += 1
+        
+
+    return output_arr
+
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# matrix = [
+#     [1, 2, 3, 4],
+#     [5, 6, 7, 8],
+#     [9, 10, 11, 12]
+# ]
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-spiralOrder(matrix)
+
+    [1, 2, 3, 4], 
+    [5, 6, 7, 8], 
+    [9, 10, 11, 12], 
+    [13, 14, 15, 16]
+    
+    ]
+
+print(spiralOrder(matrix))
