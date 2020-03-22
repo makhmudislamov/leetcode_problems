636ce737a50e0884abe1255fded53ed98366fd47
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-22 04:56:41 -0700

    pseudocode

--- microsoft/linked_list/add_2_nums.py
+++ microsoft/linked_list/add_2_nums.py
@@ -1,5 +1,7 @@
 """
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -17,9 +19,32 @@ Explanation: 342 + 465 = 807.
 
 
 class Solution(object):
+
+    # helper method1
+    # initaliza variables to store nums as str
+    # iterate over ll1:
+    # retireve val and append the num val as a str to above var
+    # return it as int
+
+    # helper meethod2 - input array
+    # iterates over string
+    # turns the item into int and saves in array
+
+    # helper method3 - should build ll
+
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
+        # num1 = helper method1
+        # num2 = helper method2
+
+        # add two nums, stringify and reverse
+        # input array - helper method2
+        # return helper method3 (input array)
+
+
+
+
