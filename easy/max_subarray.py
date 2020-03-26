028f1a05d6f5b3b22fad246d921e7f10b7a50059
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-26 08:50:50 -0700

    done

--- easy/max_subarray.py
+++ easy/max_subarray.py
@@ -13,7 +13,18 @@ If you have figured out the O(n) solution, try coding another solution
 using the divide and conquer approach, which is more subtle
 """
 
+def maxSubArray(nums: [int]) -> int:
+    n = len(nums)
+    curr_sum = max_sum = nums[0]
+    
 
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        pass
+    for i in range(1, n):
+        print(f"cur sum: {curr_sum}. max_sum: {max_sum}")
+        curr_sum = max(nums[i], curr_sum + nums[i])
+        max_sum = max(max_sum, curr_sum)
+        
+    return max_sum
+
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxSubArray(nums))
