1bd9990e3a255dfaaf2abb8e44196f4825ffad54
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-26 06:42:35 -0700

    O(n) - solution

--- easy/palindrome_ll.py
+++ easy/palindrome_ll.py
@@ -14,12 +14,27 @@ Follow up:
 Could you do it in O(n) time and O(1) space?
 """
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        pass
+        arr = []
+        while head is not None: # O(n)
+            arr.append(head.val)
+            head = head.next
+        
+        return self.isPalArr(arr)
+        
+    def isPalArr(self, array):
+        start, end = 0, len(array) - 1
+        
+        while start <= end: # O(n)
+            if array[start] != array[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
