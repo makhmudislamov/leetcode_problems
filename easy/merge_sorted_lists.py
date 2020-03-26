004940c045013d38e8b428375627166941ce5d1e
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-25 23:05:40 -0700

    done

--- easy/merge_sorted_lists.py
+++ easy/merge_sorted_lists.py
@@ -17,4 +17,28 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        if l1 is None and l2 is None:
+            return None
+        
+        temp = ListNode(0)
+        current = temp
+#         started iteration
+        while l1 is not None and l2 is not None:
+#             linking current to l1 or l2
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+#         if l1 or l2 ends early
+
+        if l1 is not None:
+            current.next = l1
+            l1 = l1.next
+        if l2 is not None:
+            current.next = l2
+            l2 = l2.next
+            
+        return temp.next
