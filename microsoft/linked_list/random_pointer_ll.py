dc727dd33a906dcd0088924f293f1de2857faaf0
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-27 23:42:41 -0700

    done

--- microsoft/linked_list/random_pointer_ll.py
+++ microsoft/linked_list/random_pointer_ll.py
@@ -34,7 +34,7 @@ Number of Nodes will not exceed 1000.
 
 
 # Definition for a Node.
-class Node:
+class ListNode:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
@@ -42,8 +42,55 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
+        if head is None:
+            return None
 
-        node = head
-        while node is not None:
-            print(node.val, node.next)
-            node = node.next
+        current = head
+        while current is not None:
+            #             adding nodes with the same values after each node
+            temp = ListNode(current.val)
+            temp.next = current.next
+            current.next = temp
+            current = current.next.next
+
+
+#             copying random pointers
+        current = head
+        while current is not None:
+            if current.random == None:
+                current.next.random = None
+            else:
+                current.next.random = current.random.next
+            current = current.next.next
+#         # not checking current.next.next == None, because interweaving creates even
+# #         number of nodes
+
+        # while head is not None:
+        #     if head.random == None:
+        #         print(f"val:{head.val}, rand:{head.random}")
+        #     else:
+        #         print(f"val:{head.val}, rand:{head.random.val}")
+        #     head = head.next
+
+        # val:7, rand:None
+        # val:7, rand:None
+        # val:13, rand:7
+        # val:13, rand:7
+        # val:11, rand:1
+        # val:11, rand:1
+        # val:10, rand:11
+        # val:10, rand:11
+        # val:1, rand:7
+        # val:1, rand:7
+
+# #       separating clone nodes to build whole copied ll
+        copied = head.next
+        current = head
+        while current.next is not None and current.next.next is not None:
+
+            temp = current.next
+            current.next = current.next.next
+            current = current.next
+            temp.next = current.next
+
+        return copied
