97353c113cea23d67606f38c4e2a7925bc2381ee
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-28 11:17:51 -0700

    optimal version

--- easy/stack_class.py
+++ easy/stack_class.py
@@ -23,23 +23,46 @@ minStack.getMin();   --> Returns -2.
 class MinStack:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.list = list()
-      
+        # init two stacks: main and min_stack to keep track of min values.
+        # All operations: O(1), space O(n)
+        self.stack = []
+        self.min_stack = []
+
     def push(self, x: int) -> None:
-        self.list.append(x)
+
+        # We always put the number onto the main stack.
+        self.stack.append(x)
+
+        # If the min stack is empty, or this number is smaller than
+        # the top of the min stack, put it on with a count of 1.
+        if not self.min_stack or x < self.min_stack[-1][0]:
+            self.min_stack.append([x, 1])
+
+        # Else if this number is equal to what's currently at the top
+        # of the min stack, then increment the count at the top by 1.
+        elif x == self.min_stack[-1][0]:
+            self.min_stack[-1][1] += 1
 
     def pop(self) -> None:
-        self.list.pop()
+
+        # If the top of min stack is the same as the top of stack
+        # then we need to decrement the count at the top by 1.
+        if self.min_stack[-1][0] == self.stack[-1]:
+            self.min_stack[-1][1] -= 1
+
+        # If the count at the top of min stack is now 0, then remove
+        # that value as we're done with it.
+        if self.min_stack[-1][1] == 0:
+            self.min_stack.pop()
+
+        # And like before, pop the top of the main stack.
+        self.stack.pop()
 
     def top(self) -> int:
-        last = self.list[len(self.list)-1]
-        return last
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.list)
+        return self.min_stack[-1][0]
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
