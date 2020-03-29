9f5b6ceed168f8bc573e882d1dcc9584c7cd0e9a
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-28 17:44:25 -0700

    used stack

--- easy/queue_class.py
+++ easy/queue_class.py
@@ -32,25 +32,34 @@ class MyQueue:
         Initialize your data structure here.
         """
 
+        self.stack = []
+
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
+        self.stack.append(x)
 
     def pop(self) -> int:
         """
-        Removes the element from in front of queue and returns that element.
+        Removes the element from in front of queue and             returns that element.
         """
+
+        return self.stack.pop(0)
 
     def peek(self) -> int:
         """
         Get the front element.
         """
+        return self.stack[0]
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
+        if len(self.stack) == 0:
+            return True
+        return False
 
 
 # Your MyQueue object will be instantiated and called as such:
