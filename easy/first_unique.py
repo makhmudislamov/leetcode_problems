9693aa36186c6c873f2d10fee68edcc33685949d
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-29 00:04:26 -0700

    done

--- easy/first_unique.py
+++ easy/first_unique.py
@@ -14,4 +14,21 @@ Note: You may assume the string contain only lowercase letters.
 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        pass
+        chars = {}
+
+        if len(s) == 0 or s is None:
+            return -1
+        
+        for i, c in enumerate(s):
+            if c not in chars:
+                chars[c] = [1, i]
+            else:
+                chars.get(c)[0] += 1
+        
+        print(chars)
+        for k in chars.keys():
+            
+            if chars.get(k)[0] == 1:
+                return chars.get(k)[1]
+        
+        return -1
