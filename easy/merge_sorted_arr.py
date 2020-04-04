33157e941727dbeb3a9e0121d49933e0cb73ae98
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-04 06:22:54 -0700

    passes some tests

--- easy/merge_sorted_arr.py
+++ easy/merge_sorted_arr.py
@@ -20,21 +20,22 @@ def merge(nums1: [int], m: int, nums2: [int], n: int) -> None:
 
     # SOLUTION 3 -  O(m+n), Space (1)
     # two pointers from the end to fill nums1 since the end is empty
-    p = len(nums1) - 1
     p1 = m - 1
-    p2 = len(nums2) - 1
+    p2 = n - 1
+    p = m + n - 1
 
-    while p1 >= 0:
+    while p1 >= 0 and p2 >= 0:
         if nums1[p1] < nums2[p2]:
             nums1[p] = nums2[p2]
             p2 -= 1
-            p -= 1
+            # p -= 1
         elif nums1[p1] > nums2[p2]:
             nums1[p] = nums1[p1]
             p1 -= 1
         else:
             nums1[p] = nums2[p2]
             p1 -= 1
+        p -= 1
 
 
         
