ebe0bdfa194ae2c02ad572449cfefe5378971707
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-07 12:21:49 -0700

    started

---
+++ intr_cake/fibonacci.py
@@ -0,0 +1,60 @@
+"""
+Write a function fib() that takes an integer nn and returns the nnth Fibonacci ↴ number.
+
+Let's say our Fibonacci series is 0-indexed and starts with 0. So:
+
+  fib(0)  # => 0
+fib(1)  # => 1
+fib(2)  # => 1
+fib(3)  # => 2
+fib(4)  # => 3
+...
+"""
+
+import unittest
+
+
+def fib(n):
+
+    # Compute the nth Fibonacci number
+
+    # Tests
+
+class Test(unittest.TestCase):
+
+    def test_zeroth_fibonacci(self):
+        actual = fib(0)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_first_fibonacci(self):
+        actual = fib(1)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_second_fibonacci(self):
+        actual = fib(2)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_third_fibonacci(self):
+        actual = fib(3)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_fifth_fibonacci(self):
+        actual = fib(5)
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_tenth_fibonacci(self):
+        actual = fib(10)
+        expected = 55
+        self.assertEqual(actual, expected)
+
+    def test_negative_fibonacci(self):
+        with self.assertRaises(Exception):
+            fib(-1)
+
+
+unittest.main(verbosity=2)
