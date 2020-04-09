16d1fcc2663cbfa9b77df3383abbb70c1b272afc
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-08 18:21:54 -0700

    started

---
+++ microsoft/teacher_n_students.py
@@ -0,0 +1,14 @@
+"""
+Teachers and students are identified by an integer that represents their proficiency level.
+Write a method to assign each student to the teacher of the closest level.
+students
+s = [ 1, 5, 8, 14, 56, 12, 0, 98, 3]
+s = [0, 1, 3, 5, 8, 12, 14, 56, 98]
+teachers
+t = [1, 2, 5, 7, 87, 14, 56]
+t = [1, 2, 5, 7, 14, 56, 87]
+you can write to stdout for debugging purposes, e.g.
+
+[(1, 1),(8, 7), (14, 14), (56, 56), (12, 14), (0,1), (98, 87), (3, 2)]
+"""
+
