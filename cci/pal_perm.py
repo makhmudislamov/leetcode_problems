d95d3df6791ea29ef19a73361639ae80698e9d54
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-10 15:28:38 -0700

    done

--- cci/pal_perm.py
+++ cci/pal_perm.py
@@ -13,3 +13,23 @@ check if the string is a permutaiton of a pal
 # if char is in the set delete it
 # if len of the set is less than or equal to 1 - permutation of a pal
 
+def is_permi_pal(s):
+    # base case
+    if len(s) <= 1:
+        return True
+    s = s.lower()
+    # "".join(s)
+    print(s)
+    check = set()
+
+    for char in s:
+        if char == " ":
+            continue
+        if char not in check:
+            check.add(char)
+        else:
+            check.remove(char)
+
+    return len(check) <= 1
+
+print(is_permi_pal("Tact Coa"))
