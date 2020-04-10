3cf1153a0a329bd50656d6110cda0d02bfca4402
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-10 15:14:42 -0700

    done

--- cci/URLify.py
+++ cci/URLify.py
@@ -8,27 +8,11 @@ output: "John%20Smith"
 
 # questions to ask:
 # acceptable and optimal time and space
+# !YOU CANNOT MODIFY STRING - STRINGS ARE IMMUTABLE
 # modifying the input? ASCII?
 
-# approach 1 - time and space >> O(n) and O(1)
+# approach 1 - time and space >> O(n) and O(n)
 # iterate over the string if the char is " " replace it with %20
-
-# def urlify(s):
-#     # base cases
-#     if not s:
-#         return s
-#     if s == " ":
-#         return "%20"
-
-#     # for i in range(len(s)):
-#     #     # print(char)
-    #     if s[i] == " ":
-    #         s[i] = "%20"
-
-    # return s
-
-# apprach 2 - O(n) and O(n)
-
 
 def urlify(s):
     # base cases
@@ -36,6 +20,22 @@ def urlify(s):
         return s
     if s == " ":
         return "%20"
-    return s.replace(" ", "%20") # returns the copy of the string
+    
+    arr = list(s) # O(n) space
+    for i in range(len(arr)): # O(n) time
+        # print(char)
+        if arr[i] == " ":
+            arr[i] = "%20"
+
+    return "".join(arr) #O(n) time
+
+# apprach 2 - O(n) and O(n)
+# def urlify(s):
+#     # base cases
+#     if not s:
+#         return s
+#     if s == " ":
+#         return "%20"
+#     return s.replace(" ", "%20") # returns the copy of the string
 
 print(urlify("John Smith Kaka"))
