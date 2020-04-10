7b3a1aa67a9439581cab16fb149f74c2004482cf
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-10 15:23:38 -0700

    started

---
+++ cci/pal_perm.py
@@ -0,0 +1,15 @@
+"""
+Chapter 1, page90
+
+check if the string is a permutaiton of a pal
+"""
+
+# questions to ask:
+#  acceptable and optimal time and space req
+# case sensetivity, lower, uppercases - input can be upper case
+
+# apprach 1 - O(n) space and time
+# use set
+# if char is in the set delete it
+# if len of the set is less than or equal to 1 - permutation of a pal
+
