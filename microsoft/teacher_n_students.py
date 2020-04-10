f022883b882699915883dd2115bdc63827cc55e7
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-09 20:05:34 -0700

    started

--- microsoft/teacher_n_students.py
+++ microsoft/teacher_n_students.py
@@ -11,4 +11,24 @@ you can write to stdout for debugging purposes, e.g.
 
 [(1, 1),(8, 7), (14, 14), (56, 56), (12, 14), (0,1), (98, 87), (3, 2)]
 """
+# whats is the min and max reqs of bigO?
+# do we have equal lenghts of lists - equal amount of teachers to students
 
+def assign(s, t):
+    res = {} # key is student and value is teacher
+
+    sorted_students = sorted(s)
+    sorted_teachers = sorted(t)
+
+    print(sorted_students)
+    print(sorted_teachers)
+
+    
+    
+
+    # return res
+
+
+s = [1, 5, 8, 14, 56, 12, 0, 98, 3]
+t = [1, 2, 5, 7, 14, 56, 87]
+print(assign(s, t))
