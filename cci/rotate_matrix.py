a7e2fd6c708c923542ae58ba69a78ea513e2e2fb
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-11 14:15:46 -0700

    pseudocode

--- cci/rotate_matrix.py
+++ cci/rotate_matrix.py
@@ -3,3 +3,12 @@ Chpater 1, page 90
 
 Given an image rep by NxN matrix, where each pixel is 4 bytes. rotate the image by 90 degrees. in place
 """
+
+# Questions to ask:
+# acceptable and optimal time and space reqs
+# rotate clockwise or counter clockwise? - assumption: clockwise
+
+# no memory approach will take O(n**2) time since we have to touch all n**2 elements
+# swapping outer most layer and then inner (alt. vice verse)
+
+
