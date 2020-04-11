a01808b84296a9942ae60790086b4c5493c103db
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-10 22:01:51 -0700

    started

---
+++ cci/string_compression.py
@@ -0,0 +1,9 @@
+"""
+Chapter1, page 90
+implement basic string compression method using counts of repeated chars.
+example
+input: aabcccccaaa
+output: a2bc5a3
+if non compressable, return the input
+input has only uppercase and lowercase letters (a-z)
+"""
