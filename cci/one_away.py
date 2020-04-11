197b55ed265f41a00a144c9db8562acc54a515cf
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-10 19:41:34 -0700

    approach one

--- cci/one_away.py
+++ cci/one_away.py
@@ -10,4 +10,43 @@ pale, bake -> False
 # space, punctations - ASCII chars?
 # lower case/upper case
 
+# base case
+# if two strings are equal -> True
+# if difference of two string len are more than 1, use abs- > False
 
+# approach 1 - O(n) space and time
+# build frequency hashtable with one string
+# loop over the second one and check
+#  if at the end len of the dict is one or less and the item freq is one -> True
+
+def one_away(s1, s2):
+    # base cases
+    if s1 == s2:
+        return True
+    if abs(len(s1) - len(s2)) > 1:
+        return False
+    
+    freq = {}
+    # building a dict
+    for char in s1:
+        if char not in freq:
+            freq[char] = 1
+        else:
+            freq[char] += 1
+    
+    for char in s2:
+        if char in freq:
+            freq[char] -= 1
+        else:
+            freq[char] = 1
+        
+        if freq[char] == 0:
+            freq.pop(char)
+
+    return not freq or len(freq) == 1
+    
+        
+s1 = "pale"
+s2 = "pleasd"
+
+print(one_away(s1, s2))
