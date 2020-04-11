1a9ab64e40ac25dedb6037550f132f3cd86f57fe
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-10 19:27:33 -0700

    started

---
+++ cci/one_away.py
@@ -0,0 +1,13 @@
+"""
+Given two strings check if they are one (or zero) edit away. Edits: insert, delete, replace a char
+Example:
+pale, ple -> True
+pale, bake -> False
+"""
+
+# questions to ask:
+# acceptable and optimal time and space reqs
+# space, punctations - ASCII chars?
+# lower case/upper case
+
+
