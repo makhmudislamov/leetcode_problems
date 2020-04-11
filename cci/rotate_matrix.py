f50a96c43d187375bc747a201c4700dbdcdea8fb
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-11 16:03:47 -0700

    step = transposing

--- cci/rotate_matrix.py
+++ cci/rotate_matrix.py
@@ -8,7 +8,28 @@ Given an image rep by NxN matrix, where each pixel is 4 bytes. rotate the image 
 # acceptable and optimal time and space reqs
 # rotate clockwise or counter clockwise? - assumption: clockwise
 
-# no memory approach will take O(n**2) time since we have to touch all n**2 elements
-# swapping diagoanlly and then using two pointers to swap elemets in each row
+# no memory approach will take O(n) time since we have to touch all n elements
+# swapping diagonally (tansposing) and then using two pointers to swap elemets in each row
+# this method works with NxN matrix
 
+def rotate_matrix(matrix):
+    matrix_len = len(matrix)
+
+    # standard looping throughh 2D arr
+    for row in range(matrix_len):
+        for col in range(row, matrix_len): # go along with row so not range(0, len)
+            # transposing - swapping diagonally
+            temp = matrix[row][col]
+            matrix[row][col] = matrix[col][row]
+            matrix[col][row] = temp
+
+    print(matrix)
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+rotate_matrix(matrix)
 
