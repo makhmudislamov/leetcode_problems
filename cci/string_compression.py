40f1b54cd676e06c5b5d4291fd6a88e7e4d4ef19
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-10 22:08:16 -0700

    apprach 1 pseudocode

--- cci/string_compression.py
+++ cci/string_compression.py
@@ -7,3 +7,17 @@ output: a2bc5a3
 if non compressable, return the input
 input has only uppercase and lowercase letters (a-z)
 """
+
+# questions to ask:
+# acceptable and optimal time and space reqs
+
+
+# base cases
+#  if input is empty return empty string
+
+# approach 1 - O(n) time and space
+# init empty dict
+# init empty string
+# build dict with char and frequency
+# loop over the dict
+# add key,value as string to output
