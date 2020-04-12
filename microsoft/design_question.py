8acb20fb0864822a25a36d5490ea5cacc26fd404
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-12 15:07:35 -0700

    started

---
+++ microsoft/design_question.py
@@ -0,0 +1,8 @@
+# Question number 2:
+    # for each customer id
+    # for each day i need to find a customer used the product_1 has the most usage for that day\
+    # customer_1 for product_1 has 40 min for day
+    # customer_2 or 3 for product_1 has 45 min for that day
+    # max_time used  (customer_id,)
+    # for each customer id I 'd calcute the sum of usage for all products that user has used
+    #
