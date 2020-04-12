1ee2ff6093771f7880467cfa14350aa212aff21b
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-11 22:43:35 -0700

    started

---
+++ cci/string_rotation.py
@@ -0,0 +1,9 @@
+"""
+Chapter 1, page 91
+
+assume you have is_substring() method that checks if one string is a subs of another
+using only one cal of is_substring() method check if s1 is a rotation of s2
+
+example
+waterbottle >> erbottlewat
+"""
