60cfea2f501dd576116cc1f4d8db3f06e2eea875
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-14 10:15:57 -0700

    started

---
+++ book_epip/arrays/reorder_array.py
@@ -0,0 +1,11 @@
+"""
+Given [int] array, reorder the array so that even ints appear first, O(1) space
+
+input: [3,5,4,6,1]
+output: [4,6,3,5,1]
+
+Questions to ask:
+should it be sorted? - assume no
+"""
+
+
