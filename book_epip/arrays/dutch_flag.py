a604f33cca4b1c287934c10ac2c3f9874ee3ccbc
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-14 16:35:39 -0700

    started

---
+++ book_epip/arrays/dutch_flag.py
@@ -0,0 +1,12 @@
+"""
+Take an arr A and index in A, rearrange the elements so that elements less than A[i] appear first, followed by equal to A[i]
+and then greater then the pivot
+example:
+A = [0,1,2,0,2,1,1]
+i = 3 >> A[3] = 0
+A = [0,0,1,2,2,1,1]
+
+or if i = 2, A[2] = 2
+A = [0,1,0,1,1,2,2] OR A = [0,0,1,1,1,2,2]
+"""
+
