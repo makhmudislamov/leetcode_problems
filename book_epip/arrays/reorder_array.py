efb85c8a5fa242f594e7127cbe10ee363b297f90
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-14 10:22:54 -0700

    O(n) time and space

--- book_epip/arrays/reorder_array.py
+++ book_epip/arrays/reorder_array.py
@@ -8,4 +8,35 @@ Questions to ask:
 should it be sorted? - assume no
 """
 
+# Approach 1 - O(n) time and space
+# init empty array
+# loop over the input
+# if item is even append to output array
+# loop again if item is zero or odd append
 
+def reorder(arr):
+    # base case
+    if not arr:
+        return arr
+
+    if len(arr) == 1:
+        return arr
+    
+    res = []
+
+    for num in arr:
+        if num != 0 and num % 2 == 0:
+            res.append(num)
+
+    if not res:
+        return arr
+
+    for num in arr:
+        if num == 0 or num % 2 != 0:
+            res.append(num)
+
+    return res
+
+
+arr = [3, 5, 5, 0, 7, 1]
+print(reorder(arr))
