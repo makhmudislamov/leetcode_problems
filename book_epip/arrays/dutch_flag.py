e9f769d177fc5a87bbf601a10b5f996553c4ac01
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-14 18:17:08 -0700

    O(n) time and const space

--- book_epip/arrays/dutch_flag.py
+++ book_epip/arrays/dutch_flag.py
@@ -9,6 +9,37 @@ A = [0,0,1,2,2,1,1]
 or if i = 2, A[2] = 2
 A = [0,1,0,1,1,2,2] OR A = [0,0,1,1,1,2,2]
 """
+# approach 3 - O(n) time and const space
+# no nested loop and in single pass grouping elements smaller and larger
+
+def dutch_flag(arr, pivot_index):
+    # base cases
+    if not arr:
+        return arr
+    if pivot_index > len(arr) - 1:
+        raise ValueError("Invalid Index")
+    pivot = arr[pivot_index]
+    # grouping smaller elements
+    smaller = 0
+    for i in range(len(arr)):
+        if arr[i] < pivot:
+            arr[smaller], arr[i] = arr[i], arr[smaller]
+            smaller += 1
+            print(f"i {i}, smaller {smaller-1}. swapped {arr[i]} and {arr[smaller-1]} >> {arr}")
+    
+    print("*** GROUPING LARGER ELEMENTS***")
+    larger = len(arr) - 1
+    for i in reversed(range(len(arr))):
+        if arr[i] > pivot:
+            arr[i], arr[larger] = arr[larger], arr[i]
+            larger -= 1
+            print(
+                f"i {i}, larger {larger}. swapped {arr[i]} and {arr[larger]} >> {arr}")
+
+    print(arr)
+arr = [0,1,2,-3,5,2,1,7,5,9,10,1]
+pivot_index = 4
+print(dutch_flag(arr, pivot_index))
 
 # Approach 2 - O(n**2) time and O(1) space
 # 2 stage iteration
@@ -18,36 +49,31 @@ A = [0,1,0,1,1,2,2] OR A = [0,0,1,1,1,2,2]
 
 # 2nd stage iteration - we start from the end and move elements greater than 
 # pivot to the end of the array
-def dutch_flag(arr, pivot_index):
-    # base cases
-    if not arr:
-        return arr
+# def dutch_flag(arr, pivot_index):
+#     # base cases
+#     if not arr:
+#         return arr
 
-    if pivot_index > len(arr) - 1:
-        raise ValueError("Invalid Index")
+#     if pivot_index > len(arr) - 1:
+#         raise ValueError("Invalid Index")
     
-    pivot = arr[pivot_index]
-    print("input", arr)
-    # 1st iteration
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)): # increasing starting index by 1 each time
-            if arr[j] < pivot:
-                arr[i], arr[j] = arr[j], arr[i]
-                print(f"i {i}, j {j}. swapped {arr[i]} and {arr[j]} >> {arr}")
-                break
-    print("*****SECOND PASS*****")
-    for i in reversed(range(len(arr))):
-        for j in reversed(range(i)):
-            if arr[j] > pivot:
-                arr[i], arr[j] = arr[j], arr[i]
-                print(f"i {i}, j {j}. swapped {arr[i]} and {arr[j]} >> {arr}")
-                break
-
-    return arr
-arr = [0,1,2,-3,5,2,1,7,5,9,10,1]
-pivot_index = 4
-
-print(dutch_flag(arr, pivot_index))
+#     pivot = arr[pivot_index]
+#     print("input", arr)
+#     # 1st iteration
+#     for i in range(len(arr)):
+#         for j in range(i+1, len(arr)): # increasing starting index by 1 each time
+#             if arr[j] < pivot:
+#                 arr[i], arr[j] = arr[j], arr[i]
+#                 print(f"i {i}, j {j}. swapped {arr[i]} and {arr[j]} >> {arr}")
+#                 break
+#     print("*****SECOND PASS*****")
+#     for i in reversed(range(len(arr))):
+#         for j in reversed(range(i)):
+#             if arr[j] > pivot:
+#                 arr[i], arr[j] = arr[j], arr[i]
+#                 print(f"i {i}, j {j}. swapped {arr[i]} and {arr[j]} >> {arr}")
+#                 break
+#     return arr
 
 # Approach 1 - O(n) space >> have 3 lists those sum of len is = len of input arr
 # less, equal and greater
