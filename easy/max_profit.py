f3c3f4f7644f625793b59d21933384cee3d1ef8e
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-18 14:47:41 -0700

    done

--- easy/max_profit.py
+++ easy/max_profit.py
@@ -20,13 +20,25 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
 
 def maxProfit(prices: [int]):
+    # O(n**2) time and const space solution
     # set some lower floor - inf
     # iterate over the input
     # substract each element from all next nums
     # keep track of the biggest prices difference
     # return the biggest difference
     # NOTE: -num1-num2
-    pass
+    if len(prices) < 1:
+        return 0
+    lower = float("-inf")
+    
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            # print("i and j are", prices[i], prices[j])
+            profit = -(prices[i]-prices[j])
+            if profit > lower:
+                lower = profit
+    
+    return 0 if lower <= 0 else lower
 
 
 prices = [7, 1, 5, 3, 6, 4]
