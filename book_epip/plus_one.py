a237725369376b9122ff3fc24687f53104763ae2
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-18 11:27:35 -0700

    started

---
+++ book_epip/plus_one.py
@@ -0,0 +1,9 @@
+"""
+Write a program that takes as input an array of digits encoding a nonegative decimal integer D
+and updates the array to represent the integer D+1.
+
+Input: [1,2,9]
+Ouput: [1,3,0]
+explanation: 129 + 1 = 130
+"""
+
