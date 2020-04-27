63baad16fe68a71c653dc0522ebcb59c9123ef0f
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-27 12:27:51 -0700

    O(n) solution

--- book_epip/arrays/multiply.py
+++ book_epip/arrays/multiply.py
@@ -1,6 +1,6 @@
 """
 input: [1,2,3] and [5,5]
-output: [6,7,1,5] >> 123 x 55
+output: [6,7,6,5] >> 123 x 55
 """
 
 def multiply(nums1, nums2):
@@ -12,4 +12,31 @@ def multiply(nums1, nums2):
     # multiply them
     # iterate over the result
     # and add each element to output arr as int
-    pass
+    num1 = ""
+    num2 = ""
+
+    res = []
+
+    for n in nums1:
+        num1 += str(n)
+    print(num1)
+    for n in nums2:
+        num2 += str(n)
+    print(num2)
+    m = int(num1) * int(num2)
+    print(m)
+    i = 0
+    while i < len(str(m)):
+        if str(m)[i] == "-":
+            res.append(-int(str(m)[i+1]))
+            i+=2
+        res.append(int(str(m)[i]))
+        i+=1
+
+    return res
+
+
+nums1 = [1,9,3,7,0,7,7,2,1]
+nums2 = [-7,6,1,8,3,8,2,5,7,2,8,7]
+
+print(multiply(nums1, nums2))
