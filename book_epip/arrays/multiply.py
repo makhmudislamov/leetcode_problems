59909c3015b73701470b17e8ae4d1c440be01caa
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-04-27 12:07:24 -0700

    pseudocode

--- book_epip/arrays/multiply.py
+++ book_epip/arrays/multiply.py
@@ -4,4 +4,12 @@ output: [6,7,1,5] >> 123 x 55
 """
 
 def multiply(nums1, nums2):
+    # O(n) time and O(m) space
+    # declare two empty string
+    # iterate over both arrays and create fill each string with 
+    # array elements
+    # turn the strings into int
+    # multiply them
+    # iterate over the result
+    # and add each element to output arr as int
     pass
